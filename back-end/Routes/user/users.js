f1866bc13c8bebdbf571535b319d8c5c8541ab0d
momagdy13 <142846384+momagdy13@users.noreply.github.com>
2024-05-06 18:22:19 +0300

    edit

--- back-end/Routes/user/users.js
+++ back-end/Routes/user/users.js
@@ -12,10 +12,9 @@ const moment = require("moment-timezone");
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://moshop24.netlify.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
